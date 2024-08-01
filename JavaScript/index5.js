1dc7a2f9160cfd14a159dc03fd990fda7b37d297
Shivi Agawal <shiviagarwal253@gmail.com>
2024-08-01 23:17:24 +0530

    Added new files

---
+++ JavaScript/index5.js
@@ -0,0 +1,17 @@
+console.log(document.body)
+console.log(document.URL)
+console.log(document.head)
+console.log(document.domain)
+console.log(document.doctype)
+console.log(document.title)
+console.log(document.body.outerHTML);
+console.log(document.body.innerHTML);
+console.log(document.forms);
+let forms = document.forms;
+console.log(typeof forms)
+let form1 = forms[0];
+console.log(typeof form1);
+console.log(form1);
+
+let scripts = document.scripts;
+console.log(scripts);
